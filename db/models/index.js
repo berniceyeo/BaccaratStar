287f62c46ef2449ba31240186148ef63d166b3ec
berniceyeo <berniceyeosuping@gmail.com>
2022-06-07 21:56:30 +0800

    feature: migration for users table

--- db/models/index.js
+++ db/models/index.js
@@ -1,37 +1,25 @@
-'use strict';
+import sequelizePackage from "sequelize";
+import allConfig from "../../sequelize.config.cjs";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../sequelize.config.cjs')[env];
+// importing models
+import initUserModel from "./user.mjs";
+
+const { Sequelize } = sequelizePackage;
+const env = process.env.NODE_ENV || "development";
+const config = allConfig[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+//initialise a new instance of sequelize
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+db.User = initUserModel(sequelize, Sequelize.DataTypes);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
